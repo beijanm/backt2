15181a8c89dc384e7e16da2e22134f74a2c28169
beijanm <beijanmoniza@gmail.com>
2024-02-02 13:37:10 -0800

    _zip column not registering

--- model/users.py
+++ model/users.py
@@ -78,27 +78,39 @@ class User(db.Model):
     _uid = db.Column(db.String(255), unique=True, nullable=False)
     _password = db.Column(db.String(255), unique=False, nullable=False)
     _dob = db.Column(db.Date)
+    _zip = db.Column(db.Integer, unique = False, nullable = True)
     
     # Defines a relationship between User record and Notes table, one-to-many (one user to many notes)
     posts = db.relationship("Post", cascade='all, delete', backref='users', lazy=True)
 
     # constructor of a User object, initializes the instance variables within object (self)
-    def __init__(self, name, uid, password="123qwerty", dob=date.today()):
+    def __init__(self, name, uid, password="123qwerty", dob=date.today(), zip = 0):
         self._name = name    # variables with self prefix become part of the object, 
         self._uid = uid
+        self._zip = zip
         self.set_password(password)
         self._dob = dob
+        
 
     # a name getter method, extracts name from object
     @property
     def name(self):
         return self._name
     
+    
     # a setter function, allows name to be updated after initial object creation
     @name.setter
     def name(self, name):
         self._name = name
     
+    @property
+    def zip(self):
+        return self._zip
+    
+    @zip.setter
+    def zip(self, zip):
+        self._zip = zip
+        
     # a getter method, extracts email from object
     @property
     def uid(self):
@@ -170,6 +182,7 @@ class User(db.Model):
             "uid": self.uid,
             "dob": self.dob,
             "age": self.age,
+            "zip": self.zip,
             "posts": [post.read() for post in self.posts]
         }
 
@@ -203,10 +216,10 @@ def initUsers():
         """Create database and tables"""
         db.create_all()
         """Tester data for table"""
-        u1 = User(name='Thomas Edison', uid='toby', password='123toby', dob=date(1847, 2, 11))
-        u2 = User(name='Nicholas Tesla', uid='niko', password='123niko', dob=date(1856, 7, 10))
-        u3 = User(name='Alexander Graham Bell', uid='lex')
-        u4 = User(name='Grace Hopper', uid='hop', password='123hop', dob=date(1906, 12, 9))
+        u1 = User(name='Thomas Edison', uid='toby', password='123toby', dob=date(1847, 2, 11), zip = 1234)
+        u2 = User(name='Nicholas Tesla', uid='niko', password='123niko', dob=date(1856, 7, 10), zip = 1234)
+        u3 = User(name='Alexander Graham Bell', uid='lex', zip = 1234)
+        u4 = User(name='Grace Hopper', uid='hop', password='123hop', dob=date(1906, 12, 9), zip = 1234)
         users = [u1, u2, u3, u4]
 
         """Builds sample user/note(s) data"""
