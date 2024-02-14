997284a7f08c3667d2ddaffed4d419162c394db4
beijanm <beijanmoniza@gmail.com>
2024-02-13 23:31:25 -0800

    luh pass to rayhan

--- model/nbas.py
+++ model/nbas.py
@@ -3,47 +3,24 @@ import http.client
 import json
 from urllib.parse import quote
 
-nba_data = []
-
-
-# Initialize jokes
-def initNba():
-    nba_data = []
-        
-# Return nba_data from external api
-
-
-def getNbaAPIData(nba):
-    conn = http.client.HTTPSConnection("api-nba-v1.p.rapidapi.com")
-    payload = ''
-    headers = {}
-    encodednba = quote(nba)
-    conn.request("GET", "/v2/current.json?key=943e1ad914msh028ea477fd00782p17daa1jsn5d479dac7106&q=" + encodednba +"&aqi=no", payload, headers)
-    res = conn.getresponse()
-    data = res.read()
-    decodedString = data.decode("utf-8")
-    j = json.loads(decodedString)
-    nbaIcon_url = ""
-    j['firstname']['lastname'] = nbaIcon_url
-    return j
-
-# Test Joke Model
-if __name__ == "__main__": 
-    initNba()  # initialize jokes
-    
-    
 import http.client
 import json
+
+def initNba():
+    global nba_data
+    nba_data = []
+
 
 def getNbaAPIData():
     conn = http.client.HTTPSConnection("api-nba-v1.p.rapidapi.com")
     headers = {
-        'X-RapidAPI-Key': '943e1ad914msh028ea477fd00782p17daa1jsn5d479dac7106',  # Make sure to replace 'YOUR_API_KEY_HERE' with your actual API key
+        'X-RapidAPI-Key': '943e1ad914msh028ea477fd00782p17daa1jsn5d479dac7106',
         'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'
     }
-    conn.request("GET", "/players/season", '', headers)  # Adjust the endpoint as necessary
+    # Assuming you want to fetch a list of players, the correct endpoint needs to be used.
+    # This is an example endpoint; you need to replace it with the correct one as per the API documentation.
+
+    conn.request("GET", "/players?search=Alex", '', headers)
     res = conn.getresponse()
     data = res.read()
     return json.loads(data.decode("utf-8"))
-
-# No need for 'if __name__ == "__main__":' block here as it's part of the model
